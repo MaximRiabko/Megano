a2ad6111f83046db532447b2864495b59ffa58b9
Никита Выскубов <vysnik@gmail.com>
2024-03-18 21:33:37 +0300

    Исправил синглтон модель добавления настрок в админку

--- megano/core/context_processors.py
+++ megano/core/context_processors.py
@@ -1,10 +1,4 @@
 from .models import SiteSettings
 
 def load_settings(request):
-    settings = {}
-    for setting in SiteSettings.objects.all():
-        if setting.is_boolean:
-            settings[setting.name] = setting.value.lower() in ['true', 'yes', '1']
-        else:
-            settings[setting.name] = setting.value
-    return {'site_settings': settings}
+    return {'site_settings': SiteSettings.load()}
--- megano/core/models.py
+++ megano/core/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-class SiteSettings(models.Model):
+# Синглтон модель
+class SingletonModel(models.Model):
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.__class__.objects.exclude(id=self.id).delete()
+        super(SingletonModel, self).save(*args, **kwargs)
+
+    @classmethod
+    def load(cls):
+        try:
+            return cls.objects.get()
+        except cls.DoesNotExist:
+            return cls()
+
+
+class SiteSettings(SingletonModel):
     name = models.CharField(max_length=100, unique=True)
     value = models.TextField()
     is_boolean = models.BooleanField(default=False)
