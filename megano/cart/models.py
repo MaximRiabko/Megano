0d59d1df9896d179e48c25777f8bb6df067dbb36
Никита Выскубов <vysnik@gmail.com>
2024-03-19 19:35:57 +0300

    Добавил заглушки сервиса корзины

--- megano/cart/models.py
+++ megano/cart/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-
-class ShoppingCart(models.Model):
-    product_name = models.CharField(max_length=100)
-    quantity = models.IntegerField(default=1)
-
-    def __str__(self):
-        return f"{self.product_name} qt.-{self.quantity}"
--- megano/cart/views.py
+++ megano/cart/views.py
@@ -1,8 +1,3 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-from .services import CartService
 
-def add_to_cart(request: HttpRequest):
-    cart_service = CartService()
-    cart_service.add_to_cart("Product 1", 2)
-    return render(request, "cart.html")
