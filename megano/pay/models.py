c36498ada934fafb1e359c8b631cc958a5f13975
Никита Выскубов <vysnik@gmail.com>
2024-04-01 16:14:30 +0300

    Убрал префиксы с _id, добавил чойз филды для полей payment и delivery в Order

--- megano/pay/models.py
+++ megano/pay/models.py
@@ -10,17 +10,29 @@ class Payment(models.Model):
     name = models.CharField(max_length=100)
 
 class Order(models.Model):
-    user_id = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT)
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-    payment_id = models.CharField(choices=Payment)
+
+    PAYMENT_CHOICES = [
+        ('cash', 'Cash'),
+        ('credit_card', 'Credit Card'),
+    ]
+    payment = models.CharField(choices=PAYMENT_CHOICES)
+
     payment_status = models.CharField(max_length=100)
-    delivery_id = models.CharField(choices=Delivery)
+
+    DELIVERY_CHOICES = [
+        ('pickup', 'Pickup'),
+        ('courier', 'Courier'),
+    ]
+    delivery = models.CharField(choices=DELIVERY_CHOICES)
+
     created_at = models.DateTimeField(auto_now_add=True)
     comment = models.CharField(max_length=200)
 
 class OrderItem(models.Model):
-    order_id = models.ForeignKey(Order, null=True, blank=True, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, null=True, blank=True, on_delete=models.PROTECT)
     price = models.FloatField()
     old_price = models.FloatField()
     count = models.IntegerField()
