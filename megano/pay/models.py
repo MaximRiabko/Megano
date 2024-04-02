c99b25d26a0d7760c19dd3507b690415a5da6168
Никита Выскубов <vysnik@gmail.com>
2024-04-02 12:54:06 +0300

    добавил TextChoices для полей payment и delivery в модели Order

--- megano/pay/models.py
+++ megano/pay/models.py
@@ -4,25 +4,22 @@ from django.db import models
 from shopapp.models import ProductSeller
 
 
+class PaymentChoices(models.TextChoices):
+    CASH = ('cash', 'Cash')
+    CREDIT_CARD = ('credit_card', 'Credit Card')
+
+class DeliveryChoices(models.TextChoices):
+    PICKUP = ('pickup', 'Pickup')
+    COURIER = ('courier', 'Courier')
+
+
 class Order(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT, related_name='users')
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-
-    PAYMENT_CHOICES = [
-        ('cash', 'Cash'),
-        ('credit_card', 'Credit Card'),
-    ]
-    payment = models.CharField(choices=PAYMENT_CHOICES, max_length=100)
-
+    payment = models.CharField(choices=PaymentChoices.choices, default=PaymentChoices.CASH, max_length=100)
     payment_status = models.CharField(max_length=100)
-
-    DELIVERY_CHOICES = [
-        ('pickup', 'Pickup'),
-        ('courier', 'Courier'),
-    ]
-    delivery = models.CharField(choices=DELIVERY_CHOICES, max_length=100)
-
+    delivery = models.CharField(choices=DeliveryChoices.choices, default=DeliveryChoices.PICKUP, max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     comment = models.CharField(max_length=200)
 
