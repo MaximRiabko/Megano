69d64a5b59383ba9e8cf30c28f2c118a6227b1dc
Easyreal <asd-rus@bk.ru>
2024-05-02 19:18:40 +0300

    add payment (fix forms)

--- megano/megano/celery.py
+++ megano/megano/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
-
+from megano.megano.settings import ON_PAYMENT
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'megano.settings')
 
-
-app = Celery('payment')
-app.config_from_object('django.conf.settings', namespace='CELERY')
-app.autodiscover_tasks()
+if ON_PAYMENT:
+    app = Celery('payment')
+    app.config_from_object('django.conf.settings', namespace='CELERY')
+    app.autodiscover_tasks()
