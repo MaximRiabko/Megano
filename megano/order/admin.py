6c775ee49eb53109d20e85866f53dffe3f17305d
Никита Выскубов <vysnik@gmail.com>
2024-05-12 09:33:28 +0300

    добавил ордер в админку

--- megano/order/admin.py
+++ megano/order/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ["product",]
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'city',
+                    'address', 'payment', 'payment_status',
+                    'delivery', 'created_at', 'comment', 'reference_num']
+    list_filter = ['payment_status', 'created_at']
+    inlines = [OrderItemInline,]
+
