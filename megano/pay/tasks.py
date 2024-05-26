fb16d65b531f49133df57b5bc7f9248c776ac762
Владимир Кирнос <asd-rus@bk.ru>
2024-05-26 11:24:19 +0300

    create transaction in order

--- megano/pay/tasks.py
+++ megano/pay/tasks.py
@@ -3,8 +3,8 @@ from pay.models import Transaction
 
 
 @app.task
-def process_payment(uuid):
-    transaction = Transaction.objects.get(uuid=uuid)
+def process_payment(pk):
+    transaction = Transaction.objects.get(pk)
     if transaction.uuid // 2 == 0 and len(transaction.uuid) == 8:
         transaction.payment_status = "paid"
         transaction.order.payment_status = "paid"
