1483ff0d5b32dbd30a4fcb17da61bda75949dbbb
Пользователь <dailos91@mail.ru>
2024-06-01 15:45:23 +0300

    other tests

--- megano/cart/tests.py
+++ megano/cart/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class CartAddTestCase(TestCase):
+    def test_cart_add(self):
+        responce = self.client.get(
+            reverse("cart:cart_add")
+        )
+        self.assertEqual(responce.status_code, 200)
+
+class AddInsideCartTestCase(TestCase):
+    def test_add_inside_cart(self):
+        responce = self.client.get(
+            reverse("cart:add_inside_cart")
+        )
+        self.assertEqual(responce.status_code, 200)
+
+class RemoveInsideCartTestCase(TestCase):
+    def test_remove_inside_cart(self):
+        responce = self.client.get(
+            reverse("cart:remove_inside_cart")
+        )
+        self.assertEqual(responce.status_code, 200)
+
+
+class CartDetailTestCase(TestCase):
+    def test_cart_detail(self):
+        responce = self.client.get(
+            reverse("cart:cart_detail")
+        )
+        self.assertEqual(responce.status_code, 200)
